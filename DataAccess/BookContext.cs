b74fa81fdff57c3538458341bbb9731814665b5c
TechWebDots <TechWebDots@gmail.com>
2021-03-22 14:02:48 +0530

    Convert ASP.NET CORE EMPTY APP TO ASP.NET CORE MVC app with Entity Framework

---
+++ DataAccess/BookContext.cs
@@ -0,0 +1,35 @@
+﻿using AspNetCoreApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreApp.DataAccess
+{
+    public class BookContext:DbContext
+    {
+        //private const string ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;Initial Catalog=Books;Integrated Security=True";
+        public DbSet<Book> Books { get; set; }
+
+        public BookContext(DbContextOptions<BookContext> options)
+            : base(options)
+        {
+        }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    base.OnConfiguring(optionsBuilder);
+        //    optionsBuilder.UseSqlServer(ConnectionString);
+        //}
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    base.OnModelCreating(modelBuilder);
+        //    //var book = modelBuilder.Entity<Book>();
+        //    //book.HasKey(p => p.BookId);
+        //    //book.Property(p => p.Title).HasMaxLength(120).IsRequired();
+        //    //book.Property(p => p.Publisher).HasMaxLength(50);
+        //}
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -2,9 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AspNetCoreApp.DataAccess;
+using AspNetCoreApp.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -16,6 +19,8 @@ namespace AspNetCoreApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(options => options.EnableEndpointRouting = false);
+            services.AddDbContext<BookContext>(options => options.UseSqlServer("name=ConnectionStrings:BookContext"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,40 +33,51 @@ namespace AspNetCoreApp
 
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
+            //app.UseMvcWithDefaultRoute();
+            app.UseMvc(routes =>
             {
-                endpoints.MapGet("/", async context =>
-                {                    
-                    await context.Response.WriteAsync("Practical Session on EF Core by TechWebDots\n");
-                    CreateDatabaseAsync(context).Wait();
-                    AddBookAsync("Book1", "publisher", context).Wait();
-                });
+                routes.MapRoute("default", "{controller=Books}/{action=Index}/{id?}");
             });
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("<h1>Practical Session on MVC setup in .NET Core by TechWebDots</h1>");
+            //});
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/", async context =>
+            //    {
+            //        await context.Response.WriteAsync("Practical Session on EF Core by TechWebDots\n");
+            //        CreateDatabaseAsync(context).Wait();
+            //        AddBookAsync("Book1", "publisher", context).Wait();
+            //    });
+            //});
         }
 
-        public static async Task CreateDatabaseAsync(HttpContext httpContext)
-        {
-            using (var context = new BooksDBContext())
-            {
-                bool created = await context.Database.EnsureCreatedAsync();
+        //public static async Task CreateDatabaseAsync(HttpContext httpContext)
+        //{
+        //    using (var context = new BookContext())
+        //    {
+        //        bool created = await context.Database.EnsureCreatedAsync();
 
-                string createdText = created ? "created\n" : "already exists\n";
-                await httpContext.Response.WriteAsync($"database {createdText}");
-            }
-        }
-        public async Task AddBookAsync(string title, string publisher, HttpContext httpContext)
-        {
-            using (var context = new BooksDBContext())
-            {
-                var book = new Book { Title = title, Publisher = publisher };
-                context.Add(book);
-                int records = await context.SaveChangesAsync();
+        //        string createdText = created ? "created\n" : "already exists\n";
+        //        await httpContext.Response.WriteAsync($"database {createdText}");
+        //    }
+        //}
+        //public async Task AddBookAsync(string title, string publisher, HttpContext httpContext)
+        //{
+        //    using (var context = new BookContext())
+        //    {
+        //        var book = new Book { Title = title, Publisher = publisher };
+        //        context.Add(book);
+        //        int records = await context.SaveChangesAsync();
 
-                await httpContext.Response.WriteAsync($"{records} record added successfully! \n");
+        //        await httpContext.Response.WriteAsync($"{records} record added successfully! \n");
 
-            }
-            await httpContext.Response.WriteAsync("");
-        }
+        //    }
+        //    await httpContext.Response.WriteAsync("");
+        //}
 
     }
 }
