cbb07c430cfc69977e5c10b0a3db9649e12e908d
TechWebDots <TechWebDots@gmail.com>
2021-05-08 23:25:09 +0530

    Global Exception Handling

---
+++ Controllers/ErrorController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreApp.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [AllowAnonymous]
+        [Route("Error")]
+        public IActionResult Error()
+        {
+            // Fetch the exception details from IExceptionHandlerPathFeature
+            var exceptionDetails = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+            ViewBag.ErrorPath = exceptionDetails.Path;
+            ViewBag.ErrorMessage = exceptionDetails.Error.Message;
+            ViewBag.ErrorSource = exceptionDetails.Error.Source;
+            ViewBag.ErrorStackTrace = exceptionDetails.Error.StackTrace;
+            return View("Error");
+            //Log exception details either in file/db/eventlog
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AspNetCoreApp.DataAccess;
+using AspNetCoreApp.Filters;
 using AspNetCoreApp.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -19,7 +20,11 @@ namespace AspNetCoreApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc(options => options.EnableEndpointRouting = false);
+            services.AddMvc(options =>
+            {
+                options.EnableEndpointRouting = false;
+                //options.Filters.Add<OperationCancelledExceptionFilter>();
+            });            
             services.AddDbContext<BookContext>(options => options.UseSqlServer("name=ConnectionStrings:BookContext"));
             services.AddTransient<IBookRepository, BookRepository>();
         }
@@ -31,6 +36,10 @@ namespace AspNetCoreApp
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
 
             app.UseRouting();
 
@@ -40,45 +49,12 @@ namespace AspNetCoreApp
                 routes.MapRoute("default", "{controller=Books}/{action=Index}/{id?}");
             });
 
+
             //app.Run(async (context) =>
             //{
             //    await context.Response.WriteAsync("<h1>Practical Session on MVC setup in .NET Core by TechWebDots</h1>");
-            //});
-
-            //app.UseEndpoints(endpoints =>
-            //{
-            //    endpoints.MapGet("/", async context =>
-            //    {
-            //        await context.Response.WriteAsync("Practical Session on EF Core by TechWebDots\n");
-            //        CreateDatabaseAsync(context).Wait();
-            //        AddBookAsync("Book1", "publisher", context).Wait();
-            //    });
-            //});
-        }
-
-        //public static async Task CreateDatabaseAsync(HttpContext httpContext)
-        //{
-        //    using (var context = new BookContext())
-        //    {
-        //        bool created = await context.Database.EnsureCreatedAsync();
-
-        //        string createdText = created ? "created\n" : "already exists\n";
-        //        await httpContext.Response.WriteAsync($"database {createdText}");
-        //    }
-        //}
-        //public async Task AddBookAsync(string title, string publisher, HttpContext httpContext)
-        //{
-        //    using (var context = new BookContext())
-        //    {
-        //        var book = new Book { Title = title, Publisher = publisher };
-        //        context.Add(book);
-        //        int records = await context.SaveChangesAsync();
-
-        //        await httpContext.Response.WriteAsync($"{records} record added successfully! \n");
-
-        //    }
-        //    await httpContext.Response.WriteAsync("");
-        //}
+            //});            
+        }       
 
     }
 }
